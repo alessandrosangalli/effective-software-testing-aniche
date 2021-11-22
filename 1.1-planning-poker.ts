8ba42f71bd31f76f020de0a0773ea84ae6ab9553
Alessandro Sangalli <sangalli.alessandro@gmail.com>
2021-11-21 23:03:41 -1000

    feat: 1.1 and 1.2 figures

---
+++ 1.1-planning-poker.ts
@@ -0,0 +1,22 @@
+
+export interface Estimate {developer: string, value: number}
+
+export default class PlanningPoker {
+  public identifyExtremes (estimates: Estimate[]): {lowestEstimate: Estimate | null, highestEstimate: Estimate | null} {
+    let lowestEstimate: Estimate | null = null
+    let highestEstimate: Estimate | null = null
+
+    for (const estimate of estimates) {
+      if (highestEstimate === null || estimate.value > highestEstimate.value) {
+        highestEstimate = estimate
+      } else if (lowestEstimate === null || estimate.value < lowestEstimate.value) {
+        lowestEstimate = estimate
+      }
+    }
+
+    return {
+      lowestEstimate,
+      highestEstimate
+    }
+  }
+}
